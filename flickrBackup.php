417ca1dae6db45cd0fd9b7b8b3d3d5fefc244d38
Jamie Kitson <jamie@kitten-x.com>
2013-08-08 12:05:31 +0100

    remove unneeded stuff

--- flickrBackup.php
+++ flickrBackup.php
@@ -92,166 +92,24 @@ function setSecret($secret)
     setFiled('secret', $secret);
 }
 
-function flickrAuthLink($class)
-{
-  return "<a class=\"$class\" href=\"getFlickr.php\">Authorise Flickr</a>";
-}
-
-function flickrCallWithRetry($params)
-{
-  $tries = 0;
-  while($tries < 3)
-  {
-    $rsp = flickrCall($params);
-    $fc = unserialize($rsp);
-
-    if ($fc['stat'] == 'ok')
-      return $fc;
-
-    $tries++;
-  }
-
-  if (is_array($fc) && array_key_exists('message', $fc)) {
-    $msg = $fc['message'];
-  } else {
-    $msg = $rsp;
-  }
-  errorExit('Flickr call failed with: '.$msg);
-}
-
-function pagePhotos($total, $params, $statFile)
-{
-  $params['page'] = 1;
-  $allPhotos = array();
-  do {
-    $params['per_page'] = $total - count($allPhotos);
-    $fc = flickrCallWithRetry($params);
-    $allPhotos = array_merge($allPhotos, $fc['photos']['photo']);
-    writeProgress("Getting photos from Flickr.", (100 * count($allPhotos) / $total), $statFile);
-    $params['page'] += 1;
-  } while((count($allPhotos) < $total) && ($params['page'] <= $fc['photos']['pages']));
-  return $allPhotos;
-}
-
-function getPhotos($count, $maxDate, $minDate, $tags, $statFile)
-{
-  $count = $count ?: DEF_COUNT;
-  writeProgress("Getting photos from Flickr.", 0, $statFile);
-
-  if ($maxDate > "")
-    $maxDate = date('Y-m-d', strtotime($maxDate) + 24 * 60 * 60); // add a day to make it inclusive, change back to MySQL datestamp as getWithoutGeoData doesn't like Unix time stamps
-  $params = array(
-    'method' => 'flickr.photos.getWithoutGeoData',
-    'sort' => 'date-taken-desc',
-    'extras' => 'date_taken,geo',
-    'max_taken_date' => $maxDate,
-    'min_taken_date' => $minDate
-  );
-
-  if ($tags > "")
-  {
-    $params['method'] = 'flickr.photos.search';
-    $params['user_id'] = 'me';
-    $params['has_geo'] = 0;
-    $params['tags'] = $tags;
-    $params['content_type'] = 7;
-  }
-
-  $photos = pagePhotos($count, $params, $statFile);
-  addUTime($photos);
-  return $photos;
-}
-
-function addUTime(&$photos)
-{
-  // bail if we've got no photos
-  if (count($photos) == 0)
-  {
-    errorExit('No Flickr photos found.');
-  }
-
-  // do expensive str date processing just once
-  foreach($photos as &$p)
-  {
-    $p[UTIME] = strtotime($p['datetaken']);
-  }
-
-  // sort photos by date taken in case flickr has fucked up or the user has overridden sort
-  sort_array_by_utime($photos);
-
-  return $photos;
-
-}
-
-function flickrAddTags($photoId, $tags)
-{
-  return flickrCall(array(
-        'method' => 'flickr.photos.addTags',
-        'photo_id' => $photoId,
-        'tags' => $tags));
-}
-
-function flickrSetGeo($photoId, $lat, $long)
-{
-  return flickrCall(array(
-        'method' => 'flickr.photos.geo.setLocation',
-        'photo_id' => $photoId,
-        'lat' => $lat,
-        'lon' => $long));
-}
-
-function getSet($setId)
-{
-  $params = array(
-      'method' => 'flickr.photosets.getPhotos',
-      'extras' => 'date_taken,geo',
-      'photoset_id' => $setId
-    );
-  $fc = flickrCallWithRetry($params);
-  $photos = $fc['photoset']['photo'];
-  addUTime($photos);
-  return $photos;
-}
-
 function gzipCall($url)
 {
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-curl_setopt($ch, CURLOPT_ENCODING, ''); // Apparently enables any encoding and auto decodes
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept-encoding: gzip"));
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$xmlresponse = curl_exec($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+    curl_setopt($ch, CURLOPT_ENCODING, ''); // Apparently enables any encoding and auto decodes
+    //curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept-encoding: gzip"));
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $xmlresponse = curl_exec($ch);
 
-if (curl_errno($ch) != 0)
-{
-    throw new Exception('Curl error: ' . curl_error($ch));
-}
+    if (curl_errno($ch) != 0)
+    {
+        throw new Exception('Curl error: ' . curl_error($ch));
+    }
 
-curl_close($ch);
-
-/*
-
-  $opts = array(
-      'http'=>array(
-      'method'=>"GET",
-      'header'=>"Accept-Encoding: gzip\r\n" .
-                "User-Agent: my program (gzip)\r\n"
-                                )
-    );
-
-  $context = stream_context_create($opts);
-
-  $xmlresponse = file_get_contents($url, false, $context);
-
-  if ($xmlresponse !== false)
-  {
-        $xmlresponse = gzdecode($xmlresponse);
-  }
-
-*/
+    curl_close($ch);
 
 //    echo $xmlresponse;
 
@@ -284,16 +142,6 @@ function getRequestToken()
 
 parse_str($line, $params);
 
-/*
-
-  $params = Array();
-  foreach (explode('&', $line) as $chunk) 
-  {
-        $param = explode("=", $chunk);
-        if (in_array($param[0], array('oauth_token', 'oauth_verifier')))
-            $params[$param[0]] = $param[1];
-    }
-*/
 //    print_r($params);
 
     if (!(isset($params['oauth_token']) && isset($params['oauth_verifier'])))
@@ -315,7 +163,6 @@ parse_str($line, $params);
 
 }
 
-// if (!testFlickr())
 switch(testFlickr() + 2 * (count($_SERVER['argv']) - 1))
 {
     case 0:
