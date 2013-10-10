f7077684aee1eafc1ce509ccd2d9c102ce602adc
Jamie Kitson <jamie@kitten-x.com>
2013-10-10 11:36:37 +0100

    add cpu and bandwidth

--- temp.php
+++ temp.php
@@ -25,7 +25,6 @@ $next = "?min=" . ($min + $span) . "&max=" . ($max + $span);
       google.setOnLoadCallback(drawChart);
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-            ['Time', 'fan speed', 'temp1', 'temp2', 'temp3'],
 <?php
 
 $lines = file('../../temp.log');
@@ -33,12 +32,32 @@ $lines = file('../../temp.log');
 //for ($i = 0; /* max(0, count($lines) - 24 * 60); */ $i < count($lines); $i++)
 foreach($lines as $line)
 {
-    $bits = explode(' ', $line);
+    $bits = explode(' ', trim($line));
     $tim = $bits[0];
     if (!isset($first))
         $first = $bits[0];
     if ((count($bits) >= 5) && ($tim <= $max) && ($tim >= $min))
-        echo sprintf("['%s', %d, %.1f, %.1f, %d],\n", date("H:i", $bits[0]), $bits[1], $bits[2], $bits[3], $bits[4]);
+    {
+        if (!isset($c))
+        {
+            $c = count($bits);
+            switch ($c)
+            {
+                case 5: echo "['Time', 'fan speed', 'temp1', 'temp2', 'temp3'],\n"; break;
+                case 8: echo "['Time', 'fan speed', 'temp1', 'temp2', 'temp3', 'CPU', 'Tx', 'Rx'],\n"; break;
+            }
+        }
+        echo "['" . date("H:i", $bits[0]) . "'";
+        for ( $i = 1; $i < $c; $i++ )
+        {
+            if ($i >= 6)
+                echo ", " . $bits[$i] / (1024);
+            else
+                echo ", " . $bits[$i];
+        }
+        echo "],\n";
+        // echo sprintf("['%s', %d, %.1f, %.1f, %d],\n", date("H:i", $bits[0]), $bits[1], $bits[2], $bits[3], $bits[4]);
+    }
 }
 
 ?>
@@ -52,11 +71,13 @@ foreach($lines as $line)
             0:{targetAxisIndex:0 /*, type: "bars"*/ },
             1:{targetAxisIndex:1},
             2:{targetAxisIndex:1},
-            3:{targetAxisIndex:1}},
+            3:{targetAxisIndex:1},
+            4:{targetAxisIndex:1},
+            5:{targetAxisIndex:0},
+            6:{targetAxisIndex:0}},
           vAxes: {
             0: {minValue: 0, maxValue: 2000, format: "# RPM"},
-            1: {minValue: 30, maxValue: 70, format: "#C"}}, 
-          isStacked: true
+            1: {minValue: 30, maxValue: 70, format: "#"}}, 
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
--- temp.sh
+++ temp.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-sensors | egrep -E "(temp[1-3]|fan1)" | awk '{print $2}' | tr -s '\n' ' ' | sed -e "s/^/\n$(date +%s) /" >> /home/jamie/misc/temp.log
+DATE=$(date +%s)
+
+TEMP=$(sensors | egrep -E "(temp[1-3]|fan1)" | awk '{print $2}' | tr -s '°C\n+' ' ')
+
+CPU=$(top -bn2 | grep Cpu | tail -n1 | awk '{print 100 - $8}')
+
+R1=`cat /sys/class/net/eth0/statistics/rx_bytes`
+T1=`cat /sys/class/net/eth0/statistics/tx_bytes`
+sleep 1
+R2=`cat /sys/class/net/eth0/statistics/rx_bytes`
+T2=`cat /sys/class/net/eth0/statistics/tx_bytes`
+TBPS=`expr $T2 - $T1`
+RBPS=`expr $R2 - $R1`
+#TKBPS=`expr $TBPS / 1024`
+#RKBPS=`expr $RBPS / 1024`
+#echo "tx eth0: $TKBPS kb/s rx eth0: $RKBPS kb/s"
+
+echo $DATE $TEMP $CPU $TBPS $RBPS
