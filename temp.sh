90bae768615e7b5dbddbe54689cc91dfab5eb6e7
Jamie Kitson <jamie@kitten-x.com>
2015-07-07 23:34:43 +0100

    fix CPU and network useage

--- temp.sh
+++ temp.sh
@@ -2,19 +2,22 @@
 
 DATE=$(date +%s)
 
-TEMP=$(sensors | egrep -E "(temp[1-3]|fan1)" | awk '{print $2}' | tr -s '°C\n+' ' ')
+TEMP=$(sensors | egrep -E "(temp[1-3]|fan1)" | sort | grep -v N | awk '{print $2}' | tr -s '°C\n+' ' ')
 
-CPU=$(top -bn2 | grep Cpu | tail -n1 | awk '{print 100 - $8}')
+CPU=$(mpstat 1 1 | grep Average | awk '{print 100 - $12}')
+#$(top -bn2 | grep Cpu | tail -n1 | awk '{print 100 - $8}')
 
 R1=`cat /sys/class/net/eth0/statistics/rx_bytes`
 T1=`cat /sys/class/net/eth0/statistics/tx_bytes`
-sleep 1
+sleep 10
 R2=`cat /sys/class/net/eth0/statistics/rx_bytes`
 T2=`cat /sys/class/net/eth0/statistics/tx_bytes`
 TBPS=`expr $T2 - $T1`
 RBPS=`expr $R2 - $R1`
-#TKBPS=`expr $TBPS / 1024`
-#RKBPS=`expr $RBPS / 1024`
+TKBPS=`expr $TBPS / 10`
+#24`
+RKBPS=`expr $RBPS / 10`
+#24`
 #echo "tx eth0: $TKBPS kb/s rx eth0: $RKBPS kb/s"
 
-echo $DATE $TEMP $CPU $TBPS $RBPS
+echo $DATE $TEMP $CPU $TKBPS $RKBPS
